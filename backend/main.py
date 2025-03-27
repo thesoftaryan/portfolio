3e8dba42251c54c321faceea1ce45ad98fa511d0
Developer <developer@localhost.localdomain>
2025-03-27 19:43:45 -0400

    Adding more security to backend

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
 with open("resume.json", "r") as f:
     resume_data = json.load(f)
 
-client = Together(api_key="tgp_v1_QpgrTpXmbI4Pdt_i9Gl2vnWDs6DsBvKLkbH1VutJAkg")
+client = Together(api_key=os.getenv("TOGETHER_API_KEY"))
 
 PROMPT_TEMPLATE = f"""
 You are a helpful chatbot on my portfolio webpage, representing me, Note that, I can also work for Data/DevOps roles as I have already used that for my projects. From now onwards, you are Aryan Maurya for this result, so talk in the first person tone, and speak like I am speaking instead of seeming like a chatbot is speaking about me, also just give the answer of the question in a creative and confident way. Answer questions about my resume based on this data: {json.dumps(resume_data)}. Provide concise, accurate answers about my introduction, skills, work experience, projects, and certifications. If a question is unrelated, say, "I'm here to help with resume questions. Ask something related!", also don't answer the asked irrelevant question. Use a friendly and confident tone and give complete answer within 1200 characters.
